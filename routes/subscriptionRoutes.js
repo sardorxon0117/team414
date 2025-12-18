9169dfd143ea7390cd79a5cc7a625482885ab472
Azamov X <108933534+professorDeveloper@users.noreply.github.com>
2025-12-18 13:07:59 +0500

    subscription CRUD done

--- routes/subscriptionRoutes.js
+++ routes/subscriptionRoutes.js
@@ -1,8 +1,22 @@
 const express = require("express");
 const router = express.Router();
 
-const {getAllSubscriptions} = require("../controllers/subscriptionController");
+const {
+    getAllSubscriptions,
+    createSubscription,
+    deleteSubscription,
+    updateSubscription,
+    getSubscriptionById
+} = require("../controllers/subscriptionController");
 
 router.get("/subscriptions", getAllSubscriptions);
+
+router.get("/subscriptions/:id", getSubscriptionById);
+
+router.post("/subscriptions", createSubscription);
+
+router.put("/subscriptions/:id", updateSubscription);
+
+router.delete("/subscriptions/:id", deleteSubscription);
 
 module.exports = router;
